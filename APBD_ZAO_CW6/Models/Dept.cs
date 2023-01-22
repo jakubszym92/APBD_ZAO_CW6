41cece7b5772d0fad2593e62426e98de5803ab2a
Jakub Szymczuk <s20289@pjwstk.edu.pl>
2023-01-22 21:48:42 +0100

    Dodaj pliki projektów.

---
+++ APBD_ZAO_CW6/Models/Dept.cs
@@ -0,0 +1,9 @@
+﻿namespace APBD_ZAO_CW6.Models
+{
+    public class Dept
+    {
+        public int Deptno { get; set; }
+        public string Dname { get; set; }
+        public string Loc { get; set; }
+    }
+}
---
+++ APBD_ZAO_CW6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APBD_ZAO_CW6
+{
+    class Program
+    {
+
+         static void show(IEnumerable<object> task)
+        {
+            foreach (var obj in task) Console.WriteLine(obj);
+            Console.WriteLine("**************************************");
+        }
+
+        static void Main(string[] args)
+        {
+            show(LinqTasks.Task1());
+            show(LinqTasks.Task2());
+            Console.WriteLine(LinqTasks.Task3());
+            Console.WriteLine("**************************************");
+            show(LinqTasks.Task4());
+            show(LinqTasks.Task5());
+            show(LinqTasks.Task6());
+            show(LinqTasks.Task7());
+            Console.WriteLine(LinqTasks.Task8());
+            Console.WriteLine("**************************************");
+            Console.WriteLine(LinqTasks.Task9());
+            Console.WriteLine("**************************************");
+            show(LinqTasks.Task10());
+            show(LinqTasks.Task11());
+            show(LinqTasks.Task12());
+            int[] array = { 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1 };            
+            Console.WriteLine(LinqTasks.Task13(array));   
+            Console.WriteLine("**************************************");
+            show(LinqTasks.Task14());
+
+        }
+    }
+}
